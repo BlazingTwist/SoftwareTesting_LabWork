518a9d14bad17250ba7ac0e33b327cfbea4bbdda
sanschul <sanschul@iti.cs.uni-magdeburg.de>
2020-11-08 22:35:01 +0100

    change in NumFinder test; added comments to leap year tests

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -6,8 +6,8 @@ public class NumFinderMain {
         NumFinder nf = new NumFinder();
 
         // this works
-        //nf.find(new int[] {4, 25, 7, 9});
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {4, 25, 7, 9});
+        //nf.find(new int[] {4, 3, 2, 1});
 
 
 
--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
+    // divisable by four, not divisable by 100
     @Test
     public void leapYearsThatAreNonCenturialYears() {
         LeapYear ly = new LeapYear();
@@ -11,18 +12,21 @@ public class LeapYearTest {
         Assertions.assertTrue(result);
     }
 
+    // divisable by 4, 100, and 400
     @Test
     public void leapCenturialYears() {
         LeapYear ly = new LeapYear();
         Assertions.assertTrue(ly.isLeapYear(2000));
     }
 
+    // divisable by 4 and 100, not divisiable by 400
     @Test
     public void nonLeapCenturialYears() {
         LeapYear ly = new LeapYear();
         Assertions.assertFalse(ly.isLeapYear(1500));
     }
 
+    // not divisable by 4
     @Test
     public void nonLeapYears() {
         LeapYear ly = new LeapYear();
