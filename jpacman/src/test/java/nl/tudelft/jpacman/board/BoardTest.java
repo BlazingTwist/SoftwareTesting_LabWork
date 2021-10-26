d38be76b533af34ea7a5d6a63a3929509bf11069
lfritzsc <lukas.fritzsche@st.ovgu.de>
2021-10-26 12:50:29 +0200

    fix checkstyle

--- jpacman/src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ jpacman/src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -2,10 +2,19 @@ package nl.tudelft.jpacman.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * A Test for validating behaviour of the {@link Board} class.
+ */
 public class BoardTest {
+    /**
+     * .
+     * Test that the board
+     * - contains as many rows / columns as expected
+     * - contains the provided square(s)
+     * - is invariant
+     */
     @Test
     public void createValidBoard() {
         BasicSquare square = new BasicSquare();
@@ -15,7 +24,7 @@ public class BoardTest {
         Board board = new Board(grid);
         assertThat(board.getHeight()).isEqualTo(1);
         assertThat(board.getWidth()).isEqualTo(1);
-        assertThat(board.squareAt(0,0)).isEqualTo(square);
+        assertThat(board.squareAt(0, 0)).isEqualTo(square);
         assertThat(board.invariant()).isTrue();
     }
 }
--- jpacman/src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ jpacman/src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -21,22 +21,31 @@ public class DirectionTest {
         assertThat(north.getDeltaY()).isEqualTo(-1);
     }
 
+    /**
+     * Do we get the correct delta when moving east?
+     */
     @Test
-    void testEast(){
+    void testEast() {
         Direction east = Direction.valueOf("EAST");
         assertThat(east.getDeltaX()).isEqualTo(1);
         assertThat(east.getDeltaY()).isEqualTo(0);
     }
 
+    /**
+     * Do we get the correct delta when moving south?
+     */
     @Test
-    void testSouth(){
+    void testSouth() {
         Direction south = Direction.valueOf("SOUTH");
         assertThat(south.getDeltaX()).isEqualTo(0);
         assertThat(south.getDeltaY()).isEqualTo(1);
     }
 
+    /**
+     * Do we get the correct delta when moving west?
+     */
     @Test
-    void testWest(){
+    void testWest() {
         Direction west = Direction.valueOf("WEST");
         assertThat(west.getDeltaX()).isEqualTo(-1);
         assertThat(west.getDeltaY()).isEqualTo(0);
