d4e077ad116e5cd693b166cf341e6f6b778c45d9
lfritzsc <lukas.fritzsche@st.ovgu.de>
2021-11-09 14:44:37 +0100

    exercise 11
    InkyTest and testData
    Expanded GhostMapParser.java for other types of Ghosts

--- jpacman/src/test/java/nl/tudelft/jpacman/npc/ghost/GhostMapParser.java
+++ jpacman/src/test/java/nl/tudelft/jpacman/npc/ghost/GhostMapParser.java
@@ -52,6 +52,15 @@ public final class GhostMapParser extends MapParser {
             case 'C':
                 grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createClyde());
                 break;
+            case 'B':
+                grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createBlinky());
+                break;
+            case 'S':
+                grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createPinky());
+                break;
+            case 'I':
+                grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createInky());
+                break;
             default:
                 super.addSquare(grid, ghosts, startPositions, x, y, c);
         }
