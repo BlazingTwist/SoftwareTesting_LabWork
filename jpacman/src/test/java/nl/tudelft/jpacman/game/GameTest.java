b3fda0ef28857689a76a540a4d331d8ef4223971
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2021-12-14 02:49:11 +0100

    refactor and refine GameTest.java

---
+++ jpacman/src/test/java/nl/tudelft/jpacman/game/GameTest.java
@@ -0,0 +1,53 @@
+package nl.tudelft.jpacman.game;
+
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.points.PointCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+
+/**
+ * A test for all branches from the Game.start() method.
+ */
+public class GameTest {
+
+    private Level levelMock = null;
+    private SinglePlayerGame singlePlayerGame = null;
+
+    /**
+     * Called before every Test.
+     */
+    @BeforeEach
+    public void setUp() {
+        levelMock = Mockito.mock(Level.class);
+        singlePlayerGame = Mockito.spy(new SinglePlayerGame(
+            Mockito.mock(Player.class),
+            levelMock,
+            Mockito.mock(PointCalculator.class)
+        ));
+    }
+
+    /**
+     * A Test that exercises all branches of Game::start();
+     */
+    @ParameterizedTest
+    @CsvSource({
+        "true, false, false, 0",
+        "false, true, false, 0",
+        "false, false, true, 0",
+        "false, false, false, 1",
+    })
+    public void test_start_branchCoverage(boolean gameAlreadyInProgress, boolean noLivingPlayers, boolean noPellets,
+                                          int expectedTimesLevelStarted) {
+        when(singlePlayerGame.isInProgress()).thenReturn(gameAlreadyInProgress);
+        when(levelMock.isAnyPlayerAlive()).thenReturn(!noLivingPlayers);
+        when(levelMock.remainingPellets()).thenReturn(noPellets ? 0 : 1);
+
+        singlePlayerGame.start();
+
+        Mockito.verify(levelMock, Mockito.times(expectedTimesLevelStarted)).start();
+    }
+}
