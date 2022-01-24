a1b84829ef1348d878a7023158d651a7e92f420e
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2022-01-24 14:18:20 +0100

    ex-45 MultiLevelLauncher.java
    create a MultiLevelLauncher that is functionally identical with the regular Launcher and extends it.

---
+++ jpacman/src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
@@ -0,0 +1,16 @@
+package nl.tudelft.jpacman;
+
+/**
+ * Creates and launches the JPacMan UI.
+ */
+public class MultiLevelLauncher extends Launcher {
+
+    /**
+     * Main execution method for the Launcher.
+     *
+     * @param args The command line arguments - which are ignored.
+     */
+    public static void main(String[] args) {
+        new MultiLevelLauncher().launch();
+    }
+}
